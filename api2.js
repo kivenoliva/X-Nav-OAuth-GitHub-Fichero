ccbc5008aeb71fcb2244cc58309eb6facbcc8afb
Kevin Oliva Muñoz <kivenoliva@gmail.com>
2015-05-19 13:42:18 +0200

    cambios para probar una cosa para la practica final 4

--- api2.js
+++ api2.js
@@ -37,7 +37,7 @@ function infoRepo() {
     var reponame = $("#repo").val();
     repo = github.getRepo(username, reponame);
     repo.show(mostrarInfo);
-    repo.contents('master', 'X-Nav-Practica-Adivina/juegos', showFiles);
+    repo.contents('master', 'juegos', showFiles);
 };
 
 function escribirEnRepo(){
